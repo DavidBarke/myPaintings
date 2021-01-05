200ab2867955eab4c5f08a8c4fbdf7ffdb5dc309
David Barkemeyer <david.barkemeyer@gmx.de>
2021-01-05 23:32:48 +0100

    Enable filtering transactions by date

--- db/interface/transactions.R
+++ db/interface/transactions.R
@@ -1,4 +1,4 @@
-#' Get All Transactions By User ID
+#' Get Transaction IDs By User ID
 #' 
 #' @template db
 #' @param user_id User ID.
@@ -6,8 +6,20 @@
 #' @family transaction
 #' 
 #' @export
-db_get_transactions_by_user_id_for_display <- function(db, user_id) {
+db_get_transaction_ids_by_filter <- function(db, user_id, date_start, date_end) {
+  date_start <- as.character(date_start)
+  date_end <- as.character(date_end)
   DBI::dbGetQuery(
+    db,
+    "SELECT rowid
+    FROM buy_sell
+    WHERE (buyer_id = ? OR seller_id = ?) AND date > ? AND date < ?",
+    params = list(user_id, user_id, date_start, date_end)
+  )$rowid
+}
+
+db_get_transaction_display <- function(db, transaction_ids) {
+  tbl <- DBI::dbGetQuery(
     db,
     "SELECT image.title AS Title, buyer.name AS Buyer, seller.name AS Seller, 
     buy_sell.date AS Date, buy_sell.price AS Price
@@ -15,8 +27,11 @@ db_get_transactions_by_user_id_for_display <- function(db, user_id) {
     INNER JOIN image ON buy_sell.image_id = image.rowid
     INNER JOIN user AS buyer ON buy_sell.buyer_id = buyer.rowid
     INNER JOIN user AS seller ON buy_sell.seller_id = seller.rowid
-    WHERE buyer_id = ? OR seller_id = ? 
-    ORDER BY buy_sell.date DESC",
-    params = list(user_id, user_id)
+    WHERE buy_sell.rowid = ?",
+    params = list(transaction_ids)
   )
+  
+  # sort "manually" as indexed query can't be used with ORDER BY
+  tibble::as_tibble(tbl) %>%
+    dplyr::arrange(desc(Date))
 }
--- modules/ui_widgets/transaction_display/transaction_display_header.R
+++ modules/ui_widgets/transaction_display/transaction_display_header.R
@@ -2,13 +2,39 @@ transaction_display_header_ui <- function(id) {
   ns <- shiny::NS(id)
   
   shiny::fluidRow(
-    shiny::column(
+    bs4Dash::box(
       width = 12,
-      bs4Dash::box(
-        width = NULL,
-        title = "myPaintings",
-        status = "primary",
-        solidHeader = TRUE
+      status = "primary",
+      solidHeader = TRUE,
+      shiny::fluidRow(
+        shiny::column(
+          width = 12,
+          shiny::dateRangeInput(
+            inputId = ns("date"),
+            label = NULL,
+            start = "2019-01-01",
+            end = "2021-12-31",
+            min = "2019-01-01",
+            max = "2021-12-31",
+            width = "100%"
+          )
+        ),
+        shiny::column(
+          width = 6,
+          shiny::actionButton(
+            inputId = ns("apply_date"),
+            label = "Apply",
+            width = "100%"
+          )
+        ),
+        shiny::column(
+          width = 6,
+          shiny::actionButton(
+            inputId = ns("reset_date"),
+            label = "Reset",
+            width = "100%"
+          )
+        )
       )
     )
   )
@@ -21,15 +47,24 @@ transaction_display_header_server <- function(id, .values) {
       
       ns <- session$ns
       
+      transaction_ids_r <- shiny::reactive({
+        db_get_transaction_ids_by_filter(
+          db = .values$db,
+          user_id = .values$user_rv()$user_id,
+          date_start = as.character(input$date[1]),
+          date_end = as.character(input$date[2])
+        )
+      })
+      
       transactions_r <- shiny::reactive({
-        db_get_transactions_by_user_id_for_display(
+        db_get_transaction_display(
           .values$db,
-          .values$user_rv()$user_id
+          transaction_ids_r()
         )
       })
       
       return_list <- list(
-        n_r = shiny::reactive(1),
+        n_r = shiny::reactive(nrow(transactions_r())),
         transactions_r = transactions_r
       )
       
