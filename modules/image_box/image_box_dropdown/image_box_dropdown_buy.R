ce5982c48ecdbc3db2f41b116e80c1cd0f6339e3
David Barkemeyer <david.barkemeyer@gmx.de>
2021-01-12 23:41:20 +0100

    Closes #25

--- modules/image_box/image_box_dropdown/image_box_dropdown_buy.R
+++ modules/image_box/image_box_dropdown/image_box_dropdown_buy.R
@@ -52,6 +52,16 @@ image_box_dropdown_buy_server <- function(id,
       
       shiny::observeEvent(input$confirm, {
         shiny::removeModal()
+        
+        db_buy_image(
+          db = db,
+          image_id = image_r()$image_id,
+          buyer_id = .values$user_rv()$user_id
+        )
+        
+        entry <- db_get_user_entry(db, .values$user_rv()$user_id)
+        .values$user_rv(entry)
+        .values$update$db_user_rv(.values$update$db_user_rv() + 1)
       })
     }
   )
